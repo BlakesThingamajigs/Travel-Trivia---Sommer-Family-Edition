21fc6775db9e5f978718e530cbafab3fd608c1de
BlakesThingamajigs <blake@blakesthingamajigs.com>
2026-07-26 15:12:32 -0500

    Initial Commit

---
+++ Travel Trivia/Travel_TriviaApp.swift
@@ -0,0 +1,32 @@
+//
+//  Travel_TriviaApp.swift
+//  Travel Trivia
+//
+//  Created by Blake Sommer on 7/26/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Travel_TriviaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Travel TriviaTests/Travel_TriviaTests.swift
@@ -0,0 +1,18 @@
+//
+//  Travel_TriviaTests.swift
+//  Travel TriviaTests
+//
+//  Created by Blake Sommer on 7/26/26.
+//
+
+import Testing
+
+struct Travel_TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
